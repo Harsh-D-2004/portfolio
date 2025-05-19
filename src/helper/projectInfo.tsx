8b96805b505f4eaa981ea8ac2f24ceb47224b84d
Harsh Doshi <119748985+Harsh-D-2004@users.noreply.github.com>
2025-05-20 00:52:40 +0530

    upp

--- src/helper/projectInfo.tsx
+++ src/helper/projectInfo.tsx
@@ -14,19 +14,19 @@ export const projects: Project[] = [
       "An AI-powered alumni-company networking platform for Training & Placement, featuring natural language query handling, semantic search, and automated outreach.",
     image:
       "https://raw.githubusercontent.com/Harsh-D-2004/Alumnet/refs/heads/main/SS/logo.png",
-    stack: ["FastAPI", "Neo4j", "Pinecone", "LangChain", "Gemini", "FAISS"],
+    stack: ["FastAPI", "Neo4j", "Pinecone", "LangChain", "LLM Integration", "Embeddings" , "Hugging Face", "Python"],
     demoUrl: "https://alumnet.vercel.app", // replace with actual demo URL if different
     githubUrl: "https://github.com/Harsh-D-2004/Alumnet",
   },
   {
-    title: "Medicine Management System",
+    title: "BudgetGPT",
     description:
-      "A Medicine management System which allows users to manage medicine records and provide dashboard for tracking them as one in pharmacies ",
+      "BudgetGPT is a chatbot application designed to handle queries related to the Budget of India passed in 2024 using a Retrieval Augmented Generation (RAG) approach.",
     image:
-      "https://raw.githubusercontent.com/Harsh-D-2004/Medicine_Management_System/refs/heads/main/images/logo_mm.png",
-    stack: ["Java", "React", "MySQL", "Springboot", "Javascript" , "HTML" , "CSS"],
+      "https://raw.githubusercontent.com/Harsh-D-2004/BudgetGPT/refs/heads/main/screenshots/logo.png",
+    stack: ["Python", "Flask", "Javascript" , "HTML" , "CSS" , "React" , "ShadCn" , "FAISS" , "LLM Integration" , "Embeddings" , "Hugging Face"],
     demoUrl: "https://alumnet.vercel.app",
-    githubUrl: "https://github.com/Harsh-D-2004/Medicine_Management_System",
+    githubUrl: "https://github.com/Harsh-D-2004/BudgetGPT",
   },
   {
     title: "Neural Mix",
@@ -37,5 +37,25 @@ export const projects: Project[] = [
     stack: ["Python", "Flask", "Electron", "Javascript" , "HTML" , "CSS" , "Material UI" , "VST SDK"],
     demoUrl: "https://alumnet.vercel.app",
     githubUrl: "https://github.com/Harsh-D-2004/Neural-Mix",
+  },
+  {
+    title: "DyslexiLearn",
+    description:
+      "DyslexiLearn is a web-based platform that provides personalized learning experiences for students with dyslexia. The application is developed using various services from IBM Cloud",
+    image:
+      "https://raw.githubusercontent.com/Harsh-D-2004/DyslexiLearn/refs/heads/main/Screenshots/logo.png",
+    stack: ["Python", "Flask", "React", "Javascript" , "HTML" , "CSS" , "Material UI" , "Deep Learning" , "Tensorflow" , "OCR"],
+    demoUrl: "https://alumnet.vercel.app",
+    githubUrl: "https://github.com/Harsh-D-2004/Neural-Mix",
+  },
+  {
+    title: "Medicine Management System",
+    description:
+      "A Medicine management System which allows users to manage medicine records and provide dashboard for tracking them as one in pharmacies ",
+    image:
+      "https://raw.githubusercontent.com/Harsh-D-2004/Medicine_Management_System/refs/heads/main/images/logo_mm.png",
+    stack: ["Java", "React", "MySQL", "Springboot", "Javascript" , "HTML" , "CSS"],
+    demoUrl: "https://alumnet.vercel.app",
+    githubUrl: "https://github.com/Harsh-D-2004/Medicine_Management_System",
   },
 ];
